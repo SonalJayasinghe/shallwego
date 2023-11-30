1b7763cebbdfba611d2476ad79261544bd10665c
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2023-11-30 07:30:19 +0530

    mobile ui improved

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,38 @@
 'use client'
-import { Button, ThemePanel } from '@radix-ui/themes'
-import { useSession } from 'next-auth/react'
-import React from 'react'
+import { Text } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
+import { FaCircleArrowRight } from "react-icons/fa6";
 
 const Home = () => {
-  const session = useSession();
+  const router = useRouter();
   return (
     <>
-    <Button>
-      Hello world
-    </Button>
-      {session.data?.user?.email && <p> {session.data.user.email} </p>}
-    </>
-  )
-}
+      <div className="flex flex-col min-h-full items-center justify-center">
+        <Text align={'center'} className=" md:text-8xl text-6xl font-bold text-pink-700">
+          {" "}
+          ARE YOU SHY TO ASKOUT?
+        </Text>
+        <Text align={'center'} className=" md:text-6xl text-3xl font-semibold text-pink-600">
+          {" "}
+          We Got You Covered
+        </Text>
 
-export default Home
+        <div role="button" onClick={() => {
+          router.push('/admin');
+          router.refresh();
+        }}>
+          {" "}
+          <div className="flex gap-4 items-center hover:translate-x-[10px] transition ease-in p-3 hover:bg-pink-600 rounded-full hover:text-slate-100">
+            <Text className="md:text-4xl text-2xl font-semibold mb-5"> Let&apos;s Create</Text>
+            <div>
+            <FaCircleArrowRight size={'40'}/>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
