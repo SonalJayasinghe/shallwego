d3a9219e3bfe40c86d459639782b2c74335b8b57
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2023-12-02 08:29:12 +0530

    available persons api build

--- app/api/available/route.ts
+++ app/api/available/route.ts
@@ -1,17 +1,37 @@
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import { AuthOptions } from "../auth/authOptions";
+import { GENDER } from "@prisma/client";
 
-export async function GET(request:NextRequest) {
-    const usersWithoutPosts = await prisma.user.findMany({
-        where: {
-          posts: {
-            none: {},
-          },
+export async function GET(request: NextRequest) {
+  const session = await getServerSession(AuthOptions);
+  if(!session?.user) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+
+  const userGender = await prisma.user.findUnique({
+    where: {
+      email: session.user.email!,
+    },
+    select: {
+      gender: true
+    }
+  });
+
+  if(!userGender) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+
+  const oppositeGender = userGender ? userGender.gender === 'FEMALE' ? "MALE" : "FEMALE" : null;
+
+  const usersWithoutPosts = await prisma.user.findMany({
+    where: {
+      posts: {
+        none: {},
+      },
+      gender: oppositeGender!,
         },
-        select: {
-            name: true,
-        }
-      });
+    select: {
+      name: true,
+    }
+  });
 
-      return NextResponse.json(usersWithoutPosts);
+  return NextResponse.json(usersWithoutPosts);
 }
